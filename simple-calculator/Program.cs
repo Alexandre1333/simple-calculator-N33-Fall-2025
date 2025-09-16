ade3f5d9f73e4066a9bac681b6fd60cb2914172e
Alexandre Labranche <alexandre.labranche@hotmail.com>
2025-09-15 23:10:41 -0400

    Project completion

--- simple-calculator/Program.cs
+++ simple-calculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Text;
+using CalculatorEngine;
 
 namespace SimpleCalculator
 {
@@ -9,26 +10,32 @@ namespace SimpleCalculator
         {
             try
             {
-                // Class to convert user input
-                InputConverter inputConverter = new InputConverter();
-
-                // Class to perform actual calculations
-                CalculatorEngine calculatorEngine = new CalculatorEngine();
+               
                 Console.WriteLine("Welcome to Simple Calculator\nEnter  your first number then press Enter:");
-                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double firstNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
                 Console.WriteLine("Enter  your Second number then press Enter:");
-                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-                Console.WriteLine("Enter operation like + or add number then press Enter:");
+                double secondNumber = InputConverter.ConvertInputToNumeric(Console.ReadLine());
+                Console.WriteLine("Enter your operator of choice, choices available are +, -, /, *, %. You can either enter the symbol of the operator or simply say the name" +
+                    " of the operator such as \"add\" or \"subtract\" :");
                 string operation = Console.ReadLine();
 
-                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                double result = CalculatorEngine.CalculatorEngine.Calculate(operation, firstNumber, secondNumber);
 
-                Console.WriteLine(result);
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.Append("The result of ");
+                stringBuilder.Append(firstNumber);
+                stringBuilder.Append(" ");
+                stringBuilder.Append(operation);
+                stringBuilder.Append(" ");
+                stringBuilder.Append(secondNumber);
+                stringBuilder.Append(" is ");
+                stringBuilder.AppendFormat("{0:F2}", result);
+                Console.WriteLine(stringBuilder.ToString());
 
             }
             catch (Exception ex)
             {
-                // Normally, we'd log this error to a file.
+                
                 Console.WriteLine(ex.Message);
             }
 
